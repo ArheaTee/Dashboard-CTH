522170fe84ca744eabafa7175a4f30d0402eb907
ArheaTee <125956559+ArheaTee@users.noreply.github.com>
2024-05-13 00:02:19 +0700

    Do at home

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 urls_data = {
-    "url1" : "http://172.31.6.56:8090/get_all_process_stations_ui/",
-    "url2" : "http://172.31.6.60:8090/get_all_process_stations_ui/",
-    "url3" :"http://172.31.6.62:8090/get_all_process_stations_ui/",
+    "url1" : "https://api.bitkub.com/api/market/ticker",
+    # "url2" : "http://172.31.6.60:8090/get_all_process_stations_ui/",
+    # "url3" :"http://172.31.6.62:8090/get_all_process_stations_ui/",
     # "url4" :"http://172.31.6.59:8090/get_all_process_stations_ui/",
     # "url5" :"http://172.31.6.64:8090/get_all_process_stations_ui/"
+    # "url6" : "http://172.31.6.65:8090/get_all_process_stations_ui/"
 }
 
 data_dic ={}
@@ -18,7 +19,16 @@ for key, url in urls_data.items():
 
 @app.route('/')
 def home():
-    return jsonify(data_dic)
+    thb_eth_data = data_dic.get('url1', {}).get('THB_ETH', {'id': None, 'last': None})
+    thb_btc_data = data_dic.get('url1', {}).get('THB_BTC', {'id': None, 'last': None})
+    return jsonify({
+        'id ETH': thb_eth_data.get('id'),
+        'last ETH': thb_eth_data.get('last')},
+        {
+        'id thb_btc' : thb_btc_data.get('id'),
+        'last thb_btc' : thb_btc_data.get('last'),
+})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
